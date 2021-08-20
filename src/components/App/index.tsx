1170a84c2f92ccad095c8c37e5be1874e986ec71
self067 <seltor@mail.ru>
2021-08-20 14:15:11 +0300

    feat: add jest

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Header from 'components/Header';
+// import Header from 'components/Header';
 import About from 'pages/About';
 import Home from 'pages/Home';
 import Page404 from 'pages/Page404';
@@ -11,7 +10,6 @@ import { NNav, StyledLink } from './styled';
 
 const activeStyle = { color: '#0508f3' };
 const App: React.FC = () => (
-  // const App: React.FC<IApp> = (appStore: IApp) => (
   <Router>
     {/* <Header /> */}
     <div>
@@ -29,35 +27,9 @@ const App: React.FC = () => (
         <Route path="/details/:id" component={Details} />
         <Route exact path="/" component={Home} />
         <Route component={Page404} />
-        {/* <Route exact path="/about">
-          <About />
-        </Route>
-        <Route path="/details/:id">
-          <Details />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route>
-          <Page404 />
-        </Route> */}
       </Switch>
     </div>
   </Router>
 );
 
-// const mapStateToProps = store => {
-//   console.log(store);
-//   return {
-//     searchResults: store.searchResults,
-//     searchStringComplete: store.searchStringComplete,
-//     orderField: store.orderField,
-//     orderDir: store.orderDir,
-//     isLoading: store.isLoading,
-//     loadError: store.loadError,
-//     limit: store.limit,
-//     page: store.page,
-//   };
-// };
-// export default connect(mapStateToProps)(App);
 export default App;
