fa487280ae4ab212e86deec09dc4189dfd9c37fb
self067 <seltor@mail.ru>
2021-08-20 16:39:55 +0300

    feat: add jest

--- test/App.test.ts
+++ test/App.test.ts
@@ -1,20 +1,48 @@
-// import { render, fireEvent, waitFor, screen } from '@testing-library/react';
-// import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from 'history';
 
-test('null', () => {
-  const n = null;
-  expect(n).toBeNull();
-  expect(n).toBeDefined();
-  expect(n).not.toBeUndefined();
-  expect(n).not.toBeTruthy();
-  expect(n).toBeFalsy();
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import '@testing-library/jest-dom';
+
+import App from 'components/App';
+
+test('full app rendering/navigating', () => {
+  render(<App />);
+  // verify page content for expected route
+  // often you'd use a data-testid or role query, but this is also possible
+  expect(screen.getByText(/you are home/i)).toBeInTheDocument();
+
+  const leftClick = { button: 0 };
+  userEvent.click(screen.getByText(/about/i), leftClick);
+
+  // check that the content changed to the new page
+  expect(screen.getByText(/you are on the about page/i)).toBeInTheDocument();
 });
 
-test('ноль', () => {
-  const z = 0;
-  expect(z).not.toBeNull();
-  expect(z).toBeDefined();
-  expect(z).not.toBeUndefined();
-  expect(z).not.toBeTruthy();
-  expect(z).toBeFalsy();
+test('landing on a bad page', () => {
+  const history = createMemoryHistory();
+  history.push('/some/bad/route');
+  render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+
+  expect(screen.getByText(/no match/i)).toBeInTheDocument();
+});
+
+test('rendering a component that uses useLocation', () => {
+  const history = createMemoryHistory();
+  const route = '/some-route';
+  history.push(route);
+  render(
+    <Router history={history}>
+      <LocationDisplay />
+    </Router>
+  );
+
+  expect(screen.getByTestId('location-display')).toHaveTextContent(route);
 });
